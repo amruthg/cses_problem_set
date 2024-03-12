321b53517b45d708be2feee74793ee740fbac671
Amruth Gadepalli <116949224+amruthg@users.noreply.github.com>
2024-03-12 17:47:05 +0530

    Update and rename apartments to apartments.cpp

--- sorting and searching/apartments.cpp
+++ sorting and searching/apartments.cpp
@@ -8,7 +8,7 @@ int main(){
     vector<long> b(m);
 
     for (int i = 0; i < n; ++i) cin >> a[i];
-	for (int i = 0; i < m; ++i) cin >> b[i];
+    for (int i = 0; i < m; ++i) cin >> b[i];
 
     sort(a.begin(),a.end());
     sort(b.begin(),b.end());
